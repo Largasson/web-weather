fc31f74be645c4df631417c3d59784104f5af726
Pavel Naydenov <pavel.snk@gmail.com>
2023-11-27 16:01:06 +0300

    вэб-погода_v.1

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from weather import weather_by_city
+
+app = Flask(__name__)  # создаем Flask переменную (экземпляр класса Flask) и передаем в нее имя нашего файла
+
+
+@app.route('/')  # функция, идущая следом за url-запросом. url - запрос / - означает главная страница
+def index():
+    weather = weather_by_city('Moscow,Russia')
+    if weather:
+        return f"Температура в городе: {weather['temp_C']} градуса Цельсия (ощущается как {weather['FeelsLikeC']})"
+    else:
+        return "Сервис погоды временно не доступен"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ weather.py
@@ -0,0 +1,25 @@
+import requests
+
+
+def weather_by_city(city_name):
+    weather_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+    param = {
+        'key': 'fad2bca2211846118b8113642232711',
+        'q': city_name,
+        'format': 'json',
+        'num_of_days': 1,
+        'showlocaltime': 'yes',
+        'lang': 'ru'
+    }
+    result = requests.get(weather_url, params=param)
+    weather = result.json()
+    if 'data' in weather:
+        if 'current_condition' in weather['data']:
+            try:
+                return weather['data']['current_condition'][0]
+            except(IndexError, TypeError):
+                return False
+    return False
+
+if __name__ == '__main__':
+    print(weather_by_city('Moscow,Russia'))
